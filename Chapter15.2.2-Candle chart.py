b1368342f48d578e4daa6b11ee6aa5387588a74d
[xzhhj01] <[hhj01hsh9901@korea.ac.kr]>
2022-08-17 15:14:06 +0900

    subname1

---
+++ Chapter15.2.2-Candle chart.py
@@ -0,0 +1,26 @@
+import pandas_datareader.data as web
+import datetime
+import matplotlib.pyplot as plt
+import mpl_finance
+import matplotlib.ticker as ticker
+
+start = datetime.datetime(2016, 3, 1)
+end = datetime.datetime(2016, 3, 31)
+skhynix = web.DataReader("000660.KS", "yahoo", start, end)
+
+fig = plt.figure(figsize=(12, 8))
+ax = fig.add_subplot(111)
+
+day_list = []
+name_list = []
+for i, day in enumerate(skhynix.index):
+    if day.dayofweek == 0:
+        day_list.append(i)
+        name_list.append(day.strftime('%Y-%m-%d') + '(Mon)')
+
+ax.xaxis.set_major_locator(ticker.FixedLocator(day_list))
+ax.xaxis.set_major_formatter(ticker.FixedFormatter(name_list))
+
+mpl_finance.candlestick2_ohlc(ax, skhynix['Open'], skhynix['High'], skhynix['Low'], skhynix['Close'], width=0.5, colorup='r', colordown='b')
+plt.grid()
+plt.show()
